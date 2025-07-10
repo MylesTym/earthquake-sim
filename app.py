2e4e2826ffbfb629511ed9c5c5bfe58f6f1ca34e
Mylestym <myles.merriweather@outlook.com>
2025-07-09 20:56:46 -0500

    Data load

--- app.py
+++ app.py
@@ -11,3 +11,13 @@ import seaborn as sns
 st.set_page_config(page_title="Earthquake Detecion Simulation", layout="centered")
 st.title("Earthquake warning system")
 st.markdown("Simulating real-time earthquakes using real data and anaomly detection")
+
+## —— Page set up ——
+st.set_page_config(page_title="Earthquake Detecion Simulation", layout="centered")
+st.title("Earthquake warning system")
+st.markdown("Simulating real-time earthquakes using real data and anaomly detection")
+
+## —— Load data ——
+@st.cache_data
+def load_data():
+    df = pd.read_csv("A3_data.csv")
